522844f6f01a40d02784769804a3a2bd10cf701f
Micah Gordon <micahgordon66@gmail.com>
2018-05-30 13:55:19 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+class App extends React.Component {
+  render () {
+    return ()
+  }
+}
+
+class Todos extends React.Component {
+  render () {
+    return ()
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
