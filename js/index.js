3da72eb7d1f6f6451f583c0c8273d692ae44264e
Micah Gordon <micahgordon66@gmail.com>
2018-06-03 17:55:54 +0300

    add feature to allow user to delete a list item

--- js/index.js
+++ js/index.js
@@ -32,15 +32,30 @@ class App extends React.Component {
     }
   }
 
+  deleteItem (e) {
+    let listItem = e.target.parentElement.children[0].textContent
+    if (this.state.doneItems.indexOf(listItem) === -1) {
+      let listItemIndex = this.state.toDoItems.indexOf(e.target.parentElement.children[0].textContent)
+      listItem = this.state.toDoItems.splice(listItemIndex, 1)[0]
+    } else {
+      let listItemIndex = this.state.doneItems.indexOf(e.target.parentElement.children[0].textContent)
+      listItem = this.state.doneItems.splice(listItemIndex, 1)[0]
+    }
+    this.setState({
+      toDoItems: this.state.toDoItems,
+      doneItems: this.state.doneItems
+    })
+  }
+
   render () {
     return (
       <div>
         <input ref={input => { this.inputText = input }} type='text' />
         <button onClick={this.addToDo.bind(this)}>Add ToDo</button>
         <h3>ToDo</h3>
-        <List handleClick={this.toggleList.bind(this)} items={this.state.toDoItems} />
+        <List items={this.state.toDoItems} handleToggleList={this.toggleList.bind(this)} handleDelete={this.deleteItem.bind(this)} />
         <h3>Done</h3>
-        <List handleClick={this.toggleList.bind(this)} items={this.state.doneItems} />
+        <List items={this.state.doneItems} handleToggleList={this.toggleList.bind(this)} handleDelete={this.deleteItem.bind(this)} />
       </div>
     )
   }
@@ -50,7 +65,7 @@ class List extends React.Component {
   render () {
     return (
       <ul>
-        {this.props.items.map((item, index) => <li key={index}><span>{item}</span><button onClick={this.props.handleClick}>Done</button></li>)}
+        {this.props.items.map((item, index) => <li key={index}><span>{item}</span><button onClick={this.props.handleToggleList}>Done</button><button onClick={this.props.handleDelete}>X</button></li>)}
       </ul>
     )
   }
