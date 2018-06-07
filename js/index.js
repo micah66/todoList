badccca6f7adbbee229a6aada7e377c19fe4c511
Micah Gordon <micahgordon66@gmail.com>
2018-06-07 16:21:31 +0300

    improve target of toggle and delete buttons

--- js/index.js
+++ js/index.js
@@ -2,21 +2,21 @@ class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      toDoItems: [],
-      doneItems: []
+      toDoItems: ['ToDo 1', 'ToDo 2', 'ToDo 3'],
+      doneItems: ['Done 1', 'Done 2', 'Done 3']
     }
   }
+
   toggleList (e) {
-    let listItem = document.getElementById(e.target.parentElement.id)
-    let listItemText = listItem.children[0].innerHTML
-    if (this.state.doneItems.indexOf(listItemText) === -1) {
-      let listItemIndex = this.state.toDoItems.indexOf(listItemText)
-      listItemText = this.state.toDoItems.splice(listItemIndex, 1)[0]
-      this.state.doneItems.push(listItemText)
+    let listItem = e.target.attributes.todo.value
+    if (this.state.doneItems.indexOf(listItem) === -1) {
+      let listItemIndex = this.state.toDoItems.indexOf(listItem)
+      listItem = this.state.toDoItems.splice(listItemIndex, 1)[0]
+      this.state.doneItems.push(listItem)
     } else {
-      let listItemIndex = this.state.doneItems.indexOf(listItemText)
-      listItemText = this.state.doneItems.splice(listItemIndex, 1)[0]
-      this.state.toDoItems.push(listItemText)
+      let listItemIndex = this.state.doneItems.indexOf(listItem)
+      listItem = this.state.doneItems.splice(listItemIndex, 1)[0]
+      this.state.toDoItems.push(listItem)
     }
     this.setState({
       toDoItems: this.state.toDoItems,
@@ -34,12 +34,12 @@ class App extends React.Component {
   }
 
   deleteItem (e) {
-    let listItem = e.target.parentElement.children[0].textContent
+    let listItem = e.target.attributes.todo.value
     if (this.state.doneItems.indexOf(listItem) === -1) {
-      let listItemIndex = this.state.toDoItems.indexOf(e.target.parentElement.children[0].textContent)
+      let listItemIndex = this.state.toDoItems.indexOf(listItem)
       listItem = this.state.toDoItems.splice(listItemIndex, 1)[0]
     } else {
-      let listItemIndex = this.state.doneItems.indexOf(e.target.parentElement.children[0].textContent)
+      let listItemIndex = this.state.doneItems.indexOf(listItem)
       listItem = this.state.doneItems.splice(listItemIndex, 1)[0]
     }
     this.setState({
@@ -55,10 +55,12 @@ class App extends React.Component {
     return (
       <div>
         <h1 id='title'>The Husband List</h1>
-        <input ref={input => { this.inputText = input }} type='text' id='addToDo' />
-        <button className='btn' id='addBtn' onClick={this.addToDo.bind(this)}>Add ToDo</button>
-        <List class='btn' id='toDoList' heading={'Things my wife told me to do:'} counter={0} items={this.state.toDoItems} btnText='Stop Nagging' handleToggleList={this.toggleList.bind(this)} handleDelete={this.deleteItem.bind(this)} />
-        <List class='btn' id='doneList' heading={'Things my wife can stop telling me to do:'} counter={this.state.toDoItems.length} items={this.state.doneItems} btnText='Nag Again' handleToggleList={this.toggleList.bind(this)} handleDelete={this.deleteItem.bind(this)} />
+        <div id='inputAddToDo'>
+          <input ref={input => { this.inputText = input }} type='text' id='addToDo' />
+          <button className='btn' id='addBtn' onClick={this.addToDo.bind(this)}>Add ToDo</button>
+        </div>
+        <List class='btn' id='toDoList' heading={'Things my wife told me to do:'} counter={1} items={this.state.toDoItems} btnText='Stop Nagging' handleToggleList={this.toggleList.bind(this)} handleDelete={this.deleteItem.bind(this)} />
+        <List class='btn' id='doneList' heading={'Things my wife can stop telling me to do:'} counter={this.state.toDoItems.length + 1} items={this.state.doneItems} btnText='Nag Again' handleToggleList={this.toggleList.bind(this)} handleDelete={this.deleteItem.bind(this)} />
       </div>
     )
   }
@@ -69,8 +71,8 @@ class List extends React.Component {
     return (
       <div id={this.props.id}>
         <h3>{this.props.heading}</h3>
-        <ul>
-          {this.props.items.map((item, index) => <li id={index + this.props.counter}><span>{item}</span><button className={this.props.class} onClick={this.props.handleToggleList}>{this.props.btnText}</button><button className={this.props.class} onClick={this.props.handleDelete}>X</button></li>)}
+        <ul className='list'>
+          {this.props.items.map((item, index) => <li id={'item' + (index + this.props.counter)}>{item}<button className={this.props.class + ' toggleBtn'} onClick={this.props.handleToggleList} todo={item}>{this.props.btnText}</button><button className={this.props.class + ' deleteBtn'} onClick={this.props.handleDelete} todo={item} /></li>)}
         </ul>
       </div>
     )
